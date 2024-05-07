fd9dd74216352a2009aad42c4231368e3ebaf61b
Fridasam <kfridasam@gmail.com>
2024-05-08 00:10:58 +0300

    first commit

---
+++ duet/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Member(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    phone = models.IntegerField(null=True)
+    Date = models.DateField(null=True)
+    
+
+
